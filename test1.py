c24062b275278ee8e1db9607b511bf8835618bee
hadi zarei <74762379+hadizarei87@users.noreply.github.com>
2020-12-07 14:25:39 +0330

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,17 @@
+a = [5,6,8,7,"hadi zarei",3,2,14]
+a[3] = "kalat"
+a.append(444)
+print(type(a))
+print (a)
+print(24*"-")
+b = (1,2,3,4)
+print(type(b))
+print(b)
+c = range(1,11)
+d = tuple(c)
+print(d)
+print(type(d))
+print(24*"-")
+e = [1,2,3,4,5]
+e.append(23)
+print(e)
